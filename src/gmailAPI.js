3be3e93d95083c6a3ba65d160be45c369451b6eb
Jack McCutchan <jack.mccutchan@gmail.com>
2020-06-11 13:50:21 -0700

    gmail api header

---
+++ src/gmailAPI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class GmailAPI extends React.Component{
+
+
+  render(){
+
+    return(
+      <div>
+        <h1>Gmail API</h1>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { GmailAPI } from './gmailAPI.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GmailAPI />
   </React.StrictMode>,
   document.getElementById('root')
 );
