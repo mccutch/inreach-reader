c93b4a4c6e5e7a1b653775db8dc2d5280465777b
Jack McCutchan <jack.mccutchan@gmail.com>
2021-11-29 19:38:05 -0800

    minor cleanup

--- src/authentication/loginWrapper.jsx
+++ src/authentication/loginWrapper.jsx
@@ -1,96 +1,70 @@
-import React from "react";
-import { getToken } from "./myJWT.js";
-import { StandardModal } from "../components/reactComponents.jsx";
+import React, { useState } from "react";
+import { getToken } from "./myJWT";
+import { StandardModal } from "../components/reactComponents";
 
 const DEMO_USERNAME = process.env.REACT_APP_DEMO_USERNAME;
 const DEMO_PW = process.env.REACT_APP_DEMOUSER_PW;
 
-export class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: null,
-      password: null,
-    };
+export function LoginForm({ onSuccess, hideModal }) {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [loginFailed, setLoginFailed] = useState(false);
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.getJWT = this.getJWT.bind(this);
-    this.handleLoginFailure = this.handleLoginFailure.bind(this);
-  }
-
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    if (this.state.username && this.state.password) {
-      this.getJWT({
-        username: this.state.username,
-        password: this.state.password,
+    if (username && password) {
+      getJWT({
+        username: username,
+        password: password,
       });
     }
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  getJWT(data) {
+  function getJWT(data) {
     getToken({
       data: data,
-      onSuccess: this.props.onSuccess,
-      onFailure: this.handleLoginFailure,
+      onSuccess: onSuccess,
+      onFailure: setLoginFailed(true),
     });
   }
 
-  handleLoginFailure() {
-    this.setState({ loginFailed: true });
-  }
-
-  render() {
-    let failureText;
-    if (this.state.loginFailed) {
-      failureText = (
+  const title = <div>Login</div>;
+  //TODO implement password recovery
+  const form = (
+    <div>
+      {loginFailed && (
         <p>
           Login failed.{" "}
           <a href="/account/password_reset/">Forgot your password?</a>
         </p>
-      );
-    }
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username or Email"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          className="form-control m-2"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="form-control m-2"
+        />
+        <button type="submit" className="btn btn-success m-2">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 
-    let title = <div>Login</div>;
-
-    let form = (
-      <div>
-        {failureText}
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username or Email"
-            onChange={this.handleChange}
-            className="form-control m-2"
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-            className="form-control m-2"
-          />
-          <button type="submit" className="btn btn-success m-2">
-            Login
-          </button>
-        </form>
-      </div>
-    );
-
-    return (
-      <StandardModal
-        hideModal={this.props.hideModal}
-        title={title}
-        body={form}
-      />
-    );
-  }
+  return <StandardModal hideModal={hideModal} title={title} body={form} />;
 }
 
 export function demoLogin({ onSuccess, onFailure }) {
