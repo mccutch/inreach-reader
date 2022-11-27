2e22a42ffaffeb36f1e0c251bd8b891bbc5685a7
Jack McCutchan <jack.mccutchan@gmail.com>
2022-11-26 17:59:41 -0800

    add all allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('DJ_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
